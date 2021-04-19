cb234918e85a4e2dc8287a4066c81b6a5cf8d7ff
yang-daniel <yang.dan@husky.neu.edu>
2021-04-18 21:36:01 -0400

    a8.61 working on connection issues

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', (req, res) => {
 
 
 require('./controllers/quizzes-controller')(app)
-require('./controllers/questions-controller')(app)
+require('./controllers/question-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
 app.listen(4000)
